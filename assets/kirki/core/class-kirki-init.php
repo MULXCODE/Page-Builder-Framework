e339c2dedb55a4befa7182258081a40885ae2a31
Collins Agbonghama <collizo4sky@gmail.com>
2021-01-19 20:19:06 +0100

    Removed date field

--- assets/kirki/core/class-kirki-init.php
+++ assets/kirki/core/class-kirki-init.php
@@ -96,7 +96,6 @@ class Kirki_Init {
 			'kirki-color'           => 'Kirki_Control_Color',
 			'kirki-color-palette'   => 'Kirki_Control_Color_Palette',
 			'kirki-custom'          => 'Kirki_Control_Custom',
-			'kirki-date'            => 'Kirki_Control_Date',
 			'kirki-dimension'       => 'Kirki_Control_Dimension',
 			'kirki-dimensions'      => 'Kirki_Control_Dimensions',
 			'kirki-editor'          => 'Kirki_Control_Editor',
